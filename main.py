9462bb8a47feb8582890a8c6891002b1e0b0bdab
Zhenchen Xu <xuz@educ.college-montreal.qc.ca>
2024-01-19 15:08:13 -0500

    wd

--- main.py
+++ main.py
@@ -19,13 +19,12 @@ class MyGame(arcade.Window):
     def on_draw(self):
         arcade.start_render()
 
-        arcade.draw_circle_filled(self.x, self.y, 20, (self.R, self.G, self.B))
+        arcade.draw_circle_filled(self.x, self.y, 20, (255, 35, 34))
         pass
     def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
         self.x = x
         self.y = y
-    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
-        self.R,self.G,self.B = 255,54,34
+
 def main():
     my_game = MyGame()
     my_game.setup()
