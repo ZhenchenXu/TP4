4a6031e59f4705260910acb693501c8b9ff058b3
Zhenchen Xu <xuz@educ.college-montreal.qc.ca>
2024-01-19 16:17:40 -0500

    sd

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ class MyGame(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Exercice #1")
 
-        pass
+        self.list_Circle = []
+        self.list_Rectangle = []
 
     def setup(self):
         pass
@@ -19,12 +20,18 @@ class MyGame(arcade.Window):
     def on_draw(self):
         arcade.start_render()
 
-        arcade.draw_circle_filled(self.x, self.y, 20, (255, 35, 34))
-        pass
-    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
+
+
+class Ball:
+    def __init__(self):
+        self.rayon = random.randint(10,30)
+    def on_mouse_motion(self):
         self.x = x
         self.y = y
 
+    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            Mygame.list_Circle.append[self.x, self.y]
 def main():
     my_game = MyGame()
     my_game.setup()
